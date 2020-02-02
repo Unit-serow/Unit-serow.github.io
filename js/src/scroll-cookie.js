f44119bef8b53dc99c4d93cda0d1d3e4e2b35914
root <root@debian.www.baidu.com>
2020-02-02 21:54:58 +0800

    Site updated: 2020-02-02 21:54:58

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Feb 02 2020 18:07:59 GMT+0800 (中国标准时间)
+// build time:Sun Feb 02 2020 21:54:54 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
